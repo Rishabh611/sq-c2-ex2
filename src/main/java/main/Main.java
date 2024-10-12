5012e1dc41083013422f455fb9f128137c780289
Rishabh <rishabsinghrock@gmail.com>
2024-10-12 13:24:13 +0530

    new commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import main.config.Parrot;
+import main.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+    }
+}
